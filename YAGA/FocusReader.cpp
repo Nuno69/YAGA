43f30bcce9d9ecffb16dcf4afa822c4f28dc8f3e
Nuno69 <nuno69a@gmail.com>
2022-08-25 21:17:54 +0200

    Improved reading of focused objects.

--- YAGA/FocusReader.cpp
+++ YAGA/FocusReader.cpp
@@ -4,12 +4,27 @@
 namespace GOTHIC_ENGINE {
 	// Add your code here . . .
 	static zCVob* LastFocusedVob = nullptr;
+	static oCItem* item = nullptr;
+	static oCNpc* npc = nullptr;
 	void FocusReaderLoop() {
 		zCVob* currentVob = player->GetFocusVob();
 		if (currentVob == nullptr) { return; }
-		if (currentVob != LastFocusedVob) {
 			LastFocusedVob = currentVob;
-			Read(string(currentVob->GetObjectName()).AToW());
+			// Maybe the vob is an item? Or an NPC? We'll see!
+			if (currentVob != LastFocusedVob) {
+				item = currentVob->CastTo<oCItem>();
+				npc = currentVob->CastTo<oCNpc>();
+				if (item != nullptr) {
+					Read(string(item->name).AToW());
+					return;
+				}
+				if (npc != nullptr) {
+					Read(string(npc->GetName(0)).AToW());
+					return;
+				}
+				else {}
+				Read(string(currentVob->GetObjectName()).AToW());
+				return;
+			}
 		}
-}
 }
