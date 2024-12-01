504df57c34d5f5c536ee54875d266ca2268dc00b
Patrix <patrixon9999@wp.pl>
2024-12-02 00:52:21 +0100

    Added rotating towards specific rotation by holding SHIFT and pressing left or right arrow:
    - Added `RotateTowardsCompassAngle` function, function expects either `-1` or `1` as argument
    - Added `compassAngleOffset` variable, for now it is set to const, but this should be changed in the future (maybe someone would like to customize the compas offset angle)
    - Added `Hook_oCAIHuman_PC_Turnings` for disabling turning player while holding shift key

--- YAGA/CompassReader.cpp
+++ YAGA/CompassReader.cpp
@@ -7,5 +7,27 @@ namespace GOTHIC_ENGINE {
 			int compassAngle = GetCompassAngle();
 			Read(GetCompassName(compassAngle).AToW());
 		}
+
+		if (zKeyPressed( KEY_LSHIFT ) || zKeyPressed(KEY_RSHIFT)) {
+			if (zKeyToggled(KEY_LEFTARROW)) {
+				RotateTowardsCompassAngle(-1);
+
+				int compassAngle = GetCompassAngle();
+				Read(GetCompassName(compassAngle).AToW());
+			}
+
+			if (zKeyToggled(KEY_RIGHTARROW)) {
+				RotateTowardsCompassAngle(1);
+
+				int compassAngle = GetCompassAngle();
+				Read(GetCompassName(compassAngle).AToW());
+			}
+		}
+	}
+
+	HOOK Hook_oCAIHuman_PC_Turnings AS(&oCAIHuman::PC_Turnings, &oCAIHuman::PC_Turnings_Union);
+	void oCAIHuman::PC_Turnings_Union(zBOOL forceRotation) {
+		if (!zKeyPressed(KEY_LSHIFT) && !zKeyPressed(KEY_RSHIFT))
+			THISCALL(Hook_oCAIHuman_PC_Turnings)(forceRotation);
 	}
 }
