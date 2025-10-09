6e07a818446194b6141862dce9285cce6cfe8155
Patrix <patrixon9999@wp.pl>
2025-10-09 19:26:25 +0200

    Moved `GetCompassName` function to `Compass.hpp` (previously it was defined in `Utils/Generic.hpp`)

--- src/Utils/Generic.hpp
+++ src/Utils/Generic.hpp
@@ -1,19 +1,4 @@
 namespace GOTHIC_NAMESPACE {
-	std::string GetCompassName(float _angle)
-	{
-		int angle = static_cast<int>(_angle - 22);
-
-		if (angle < 45) return std::format("North at {}", angle);
-		if (angle < 90) return std::format("North-east at {}", angle);
-		if (angle < 135) return std::format("East at {}", angle);
-		if (angle < 180) return std::format("South-east at {}", angle);
-		if (angle < 225) return std::format("South at {}", angle);
-		if (angle < 270) return std::format("South-west at {}", angle);
-		if (angle < 315) return std::format("West at {}", angle);
-		if (angle < 360) return std::format("North-west at {}", angle);
-		return std::format("North at {}", angle);
-	}
-
 	float GetAngleRelation(zCVob* source, zCVob* target)
 	{
 
